ecd531865a0d55d64f7077b4d3e9185916fa44c9
Kalyan Basso <kalyan.basso@angellira.com>
2023-02-12 02:11:15 -0300

    clean

--- src/database/init.ts
+++
@@ -1,9 +0,0 @@
-import { TimeRecords } from '../models'
-
-const isDev = process.env.NODE_ENV === 'development'
-
-const dbInit = () => Promise.all([
-	TimeRecords.sync({ alter: isDev })
-])
-
-export default dbInit
--- src/models/index.ts
+++
@@ -1,5 +0,0 @@
-import TimeRecords from './TimeRecord'
-
-export {
-	TimeRecords
-}
